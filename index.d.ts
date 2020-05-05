e500bfd023a79edb23b69ac622ef38c2ae8d973b
Paul Miller <paul@paulmillr.com>
2020-05-05 22:25:08 +0300

    Fix signature

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 declare const should: {
-  (message: string, test: () => void|Promise<boolean>);
-  only(message: string, test: () => void|Promise<boolean>);
-  skip(message: string, test: () => void|Promise<boolean>);
+  (message: string, test: () => void|Promise<void>);
+  only(message: string, test: () => void|Promise<void>);
+  skip(message: string, test: () => void|Promise<void>);
   run();
 }
 
